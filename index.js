978fc1fe6c28bc02bfacde79a48dc76d0f99246e
DarkWinzo <darkwinzo4440@gmail.com>
2023-09-14 13:17:21 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,27 @@
+const cricket = require('./Data/cricket-details')
+
+class cricket {
+    constructor(match_url = '') {
+        this.match_url = match_url
+    }
+    async match_url() {
+        return await cricket.match_url()
+    }
+    async live_score(match_url) {
+        return await cricket.live_score(match_url)
+    }
+    async match_news() {
+        return await cricket.match_news()
+    }
+    async feature_match() {
+            return await cricket.feature_match()
+    }
+    async upcoming_match() {
+            return await cricket.upcoming_match()
+    }
+    async recently_match() {
+            return await cricket.recently_match()
+    }
+}
+
+module.exports = cricket
