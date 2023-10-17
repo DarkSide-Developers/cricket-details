f3f8857260c57336f7c45fe1559370485a0994ee
DarkWinzo <darkwinzo4440@gmail.com>
2023-10-17 12:16:20 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
+/**
+ * @Project_Name : Cricket-Details
+ * @Project_Author : DarkWinzo
+ * @Project_Version : 1.0.0
+ * @Project_Details : Cricket Unofficial Scraper
+ */
+
 const { cricket } = require('./cricket/data.js')
 
-module.exports = { cricket }
+module.exports = { cricket };
